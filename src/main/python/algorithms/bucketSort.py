4dc7a77cc3e035854df5ccc5568ca0536215036a
Abhishek <abhi.ashwin@gmail.com>
2021-10-25 21:45:36 -0400

    removed commented out code

--- src/main/python/algorithms/bucketSort.py
+++ src/main/python/algorithms/bucketSort.py
@@ -18,6 +18,3 @@ def bucketSort(array, *args):
             yield array, k, -1, i, -1
             array[k] = bucket[i][j]
             k += 1
-
-# for a in bucketSort([1, 125, 34, 12]):
-#     print(a)
